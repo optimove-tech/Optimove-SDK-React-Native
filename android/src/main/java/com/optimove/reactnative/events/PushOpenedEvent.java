3abe79fc99fd705bb41ed54c74033e7c32b054bc
Konstantin Antipochkin <konstantin_a@optimove.com>
2023-08-07 20:28:18 -0400

    Fixed the parsing of the push data passed from Android to native

--- android/src/main/java/com/optimove/reactnative/events/PushOpenedEvent.java
+++ android/src/main/java/com/optimove/reactnative/events/PushOpenedEvent.java
@@ -1,6 +1,7 @@
 package com.optimove.reactnative.events;
 
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,9 +9,11 @@ import androidx.annotation.Nullable;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.optimove.android.optimobile.PushMessage;
+import com.optimove.reactnative.JSONtoMapMapper;
 
 public class PushOpenedEvent implements ReactEvent {
 
+  private static final String TAG = PushOpenedEvent.class.getName();
   @NonNull
   private final PushMessage push;
   @Nullable
@@ -44,7 +47,11 @@ public class PushOpenedEvent implements ReactEvent {
     map.putInt("id", push.getId());
     map.putString("title", push.getTitle());
     map.putString("message", push.getMessage());
-    map.putString("data", push.getData().toString());
+    try {
+      map.putMap("data", JSONtoMapMapper.jsonToReact(push.getData()));
+    } catch (Throwable e) {
+      Log.e(TAG, String.format("Couldn't parse push data due to: %s", e.getMessage()));
+    }
     map.putString("url", url != null ? url.toString() : null);
 
     return map;
--- android/src/main/java/com/optimove/reactnative/events/PushReceivedEvent.java
+++ android/src/main/java/com/optimove/reactnative/events/PushReceivedEvent.java
@@ -1,15 +1,18 @@
 package com.optimove.reactnative.events;
 
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.optimove.android.optimobile.PushMessage;
+import com.optimove.reactnative.JSONtoMapMapper;
 
 public class PushReceivedEvent implements ReactEvent {
 
+  private static final String TAG = PushReceivedEvent.class.getName();
   @NonNull
   private final PushMessage push;
 
@@ -25,7 +28,11 @@ public class PushReceivedEvent implements ReactEvent {
     map.putInt("id", push.getId());
     map.putString("title", push.getTitle());
     map.putString("message", push.getMessage());
-    map.putString("data", push.getData().toString());
+    try {
+      map.putMap("data", JSONtoMapMapper.jsonToReact(push.getData()));
+    } catch (Throwable e) {
+      Log.e(TAG, String.format("Couldn't parse push data due to: %s", e.getMessage()));
+    }
     map.putString("url", url != null ? url.toString() : null);
 
     return map;
